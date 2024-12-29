370318cb8faf2366301bf8ed31e53f2bfed8f350
Damian Tchorzewski <damiantchorzewski0@gmail.com>
2024-12-29 17:40:12 +0100

    edit

---
+++ js/swiper.js
@@ -0,0 +1,14 @@
+function initializeCertSwiper() {
+  new Swiper(".mySwiper", {
+    effect: "cards",
+    grabCursor: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+}
